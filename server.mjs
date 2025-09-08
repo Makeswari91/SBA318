77661b5016f461590414e8ee0c7c8ec11d4b4a8d
Makeswari Ramanathan <mahithamakeswari91@gmail.com>
2025-09-08 15:07:01 -0400

    middleware

--- server.mjs
+++ server.mjs
@@ -6,8 +6,15 @@ const app = express();
 const PORT = 3000;
 
 //middleware
+app.use(express.json());
+
+//route
+app.get('/', (req, res) => {
+  res.send('Testing');
+});
 
 //globalerror handling
+
 
 //server listener
 app.listen(PORT, () => 
