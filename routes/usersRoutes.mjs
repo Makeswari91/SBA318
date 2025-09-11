87488482d85763dfb19bb569b0072167c0a3a5ed
Makeswari Ramanathan <mahithamakeswari91@gmail.com>
2025-09-11 17:08:51 -0400

    post request

--- routes/usersRoutes.mjs
+++ routes/usersRoutes.mjs
@@ -13,13 +13,27 @@ router
 // show one user route
 
 router
-  .route("/")
+  .route("/:id")
   .get((req,res)=>{
     const  user = users.find((user) => user.id == req.params.id);
-    if(user) res.json(user)
-        else throw new Error(`User not found`);
+    if(user){
+       res.json(user);
+    }
+        else {
+          res.status(404).json({error: 'User not found'})
+        };
 })
+     .post((req,res)=>{
+          let {name, username, age}= req.body;
+    let newentry = {id: users.length,
+        name,
+        username,
+        age
+    }
+    users.push(newentry)
+    res.status(201).json({msg: 'New entry created', item: newentry});
 
+})
 
 
 export default router;
